ffb46248c150b21faf7286b0dd4a9caff20c95a6
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-12-03 02:00:39 -0500

    login and user ok

--- app/src/main/java/com/utp/iloveauctionmovil/AuthActivity.kt
+++ app/src/main/java/com/utp/iloveauctionmovil/AuthActivity.kt
@@ -14,28 +14,36 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-        val ingresar: Button = findViewById(R.id.ingresar)
-
+        setup()
     }
+
 
     fun siguienteRegistro(view: View) {
-        val intent = Intent(this, RegisterActivity::class.java)
-        startActivity(intent)
+        val intent1 = Intent(this, RegisterActivity::class.java)
+        startActivity(intent1)
     }
 
+   /* fun siguientePrincipal(view: View) {
+        val intent2 = Intent(this, PrincipalActivity::class.java)
+        startActivity(intent2)
+    }*/
+
     private fun setup() {
-        val ingresar: Button = findViewById(R.id.ingresar)
-        val email: EditText = findViewById(R.id.email)
+        val login: Button = findViewById(R.id.login)
+        val email: EditText = findViewById(R.id.email3)
         val clave: EditText = findViewById(R.id.Password)
         title = "autenticacion"
-        ingresar.setOnClickListener {
+        login.setOnClickListener {
             if (email.text.isNotEmpty() && clave.text.isNotEmpty()) {
                 FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(email.text.toString(), clave.text.toString())
+                    .signInWithEmailAndPassword(email.text.toString(), clave.text.toString())
                     .addOnCompleteListener {
                         if (it.isSuccessful) {
-                        }
-                        else{
+                            showHome(
+                                it.result?.user?.email ?: "",
+                                PrincipalActivity.ProviderType.BASIC
+                            )
+                        } else {
                             showalert()
                         }
                     }
@@ -43,17 +51,21 @@ class AuthActivity : AppCompatActivity() {
         }
     }
 
-    private fun showalert(){
+    private fun showalert() {
 
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage("Se ha producido un error al autenticar al usuario")
-        builder.setPositiveButton("Aceptar",null)
-        val dialog:AlertDialog=builder.create()
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
         dialog.show()
     }
-
-
-
+    private fun showHome(email: String, provider: PrincipalActivity.ProviderType) {
+        val homeIntent = Intent(this, PrincipalActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
 }
 
--- app/src/main/java/com/utp/iloveauctionmovil/RegisterActivity.kt
+++ app/src/main/java/com/utp/iloveauctionmovil/RegisterActivity.kt
@@ -3,17 +3,63 @@ package com.utp.iloveauctionmovil
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        setup()
     }
 
     fun auth (view: View){
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun setup() {
+        val register: Button = findViewById(R.id.registrar)
+        val email: EditText = findViewById(R.id.email3)
+        val clave: EditText = findViewById(R.id.password)
+        title = "autenticacion"
+        register.setOnClickListener {
+            if (email.text.isNotEmpty() && clave.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(email.text.toString(), clave.text.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(
+                                it.result?.user?.email ?: "",
+                                PrincipalActivity.ProviderType.BASIC
+                            )
+                        } else {
+                            showalert()
+                        }
+                    }
+            }
+        }
+
+    }
+
+    private fun showalert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error al autenticar al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showHome(email: String, provider: PrincipalActivity.ProviderType) {
+        val homeIntent = Intent(this, AuthActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 
 
